b0f2e6266db976d17f94a6d07f20025e45e7f293
Thomas Rumas <thomas@rumas.fr>
2026-01-27 11:04:18 +0100

    feat(astro): add dark theme

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE = {
   title: "Hello ! Welcome to my personnal blog",
   description: "I'm currently a Developer Advocate at ADEO. I love to share my knowledge about Front-End Development, Cloud and Design through articles, talks and workshops.",
   author: "Thomas Rumas",
-  authorImage: "/img/logo.jpg",
+  authorImage: "/img/logo.png",
   aboutAuthor: "A curious guy about software engineering, cloud computing, developer experience and also cycling and photography",
   baseUrl: "https://thomas.rumas.fr",
   social: {
